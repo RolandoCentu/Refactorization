d30713f14600d894afd6ec1e53c39e703e328955
Rolando Centurión <rolandocena@gmail.com>
2026-02-10 14:08:41 -0300

    Add files via upload

---
+++ constantes.py
@@ -0,0 +1,19 @@
+# Emojis
+BORDE       = "🟫"
+CELDA_VACIA = "⬜"
+AGUA        = "🌊"
+EDIFICIO    = "🏢"
+BLOQUEADO   = "⛔"
+INICIO      = "🚩"
+META        = "🏁"
+CAMINO      = "🟢"
+VISITADO    = "👣"
+
+# Conjunto de obstáculos impasables
+OBSTACULOS = {BORDE, EDIFICIO, BLOQUEADO}
+
+# Diccionario de costos de paso (solo para celdas transitables)
+COSTO = {
+    CELDA_VACIA: 1,
+    AGUA: 3,
+}
